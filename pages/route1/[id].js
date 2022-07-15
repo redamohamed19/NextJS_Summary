5a99e31eb5d1e65815f28634466f0040c1661613
Sayah Reda <reda1998.rs@gmail.com>
2022-07-15 22:34:18 +0100

    Dynamic Route Parameters in NextJs

--- pages/route1/[id].js
+++ pages/route1/[id].js
@@ -1,8 +1,14 @@
+import { useRouter } from 'next/router';
+
 export default function Route() {
   //this page shown with her folderpath plus a parameters
+
+  //this is a react hook to acces the parameters
+  const router = useRouter();
+  const { id } = router.query;
   return (
     <div>
-      <h1>Index page Next jS Route 01 with parameters id</h1>
+      <h1>Index page Next jS Route 01 with parameters id fetched by id {id}</h1>
     </div>
   );
 }
