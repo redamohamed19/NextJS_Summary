eec6db0c4f72ff16c2811d8c808245f854b5374b
Sayah Reda <reda1998.rs@gmail.com>
2022-07-16 11:07:51 +0100

    Dynamic Page Navigation with the Link Component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+import Link from 'next/link';
 export default function Home() {
   //this page can displayed with her folder path
   return (
     <div>
       <h1>Index page Next jS</h1>
+      {/*this routes is just for client side rerendring */}
+      <Link href="/route1">
+        <a>Go to route1 routes Via Next Link</a>
+      </Link>
+
+      <Link href="/route1/[id]" as={'/route1/2'}>
+        <a>Navigate with dynamic routes </a>
+      </Link>
     </div>
   );
 }
